158e4993c46d97e800953ca82213572f350adf53
Karin Lampesberger <karin@lampesberger.at>
2013-10-27 10:10:19 +0100

    Aktuellster Stand

---
+++ cg2-a01/radius_dragger.js
@@ -0,0 +1,9 @@
+/* Module: radius_dragger
+	A RadiusDragger is a drawable object that can react to events
+	from a SceneController. It will typically control the position
+	of the circle of a scene
+*/
+
+// requireJS module definition
+
+
