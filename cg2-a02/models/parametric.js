8da08ea559621a58b2d9ca8cd01564562914c73b
Karin Lampesberger <karin@lampesberger.at>
2013-11-13 12:16:47 +0100

    Parametric Surface draw und constructor

--- cg2-a02/models/parametric.js
+++ cg2-a02/models/parametric.js
@@ -23,15 +23,59 @@ define(["vbo"],
      */ 
     var ParametricSurface = function(gl, posFunc, config) {
             
-        window.console.log("ParametricSurface() constructor not implemented yet.")
-           
+        //window.console.log("ParametricSurface() constructor not implemented yet.")
+        var umin = config.uMin || -Math.PI;
+        var umax = config.uMax ||  Math.PI;
+        var vmin = config.vMin || -Math.PI;
+        var vmax = config.vMax ||  Math.PI;
+        var usegments = config.uSegments || 40;
+        var vsegments = config.vSegments || 20;
+        this.posFunc = posFunc;
+
+        console.log("Creating a ParametricSurface with umin="+umin+", umax="+umax+", vmin="+vmin+", vmax="+vmax ); 
+    
+        // generate vertex coordinates and store in an array
+        var coords = [];
+
+        var umin_temp = 0;
+        var vmin_temp = 0;
+
+        for (var i=0; i <= usegments; i++) { // hier evtl usegments & vsegments noch vertauschen???
+            umin_temp += (umax - umin) / usegments;
+            vmin_temp = vmin;
+            for (var j=0; j <= vsegments; j++) {
+
+                vmin_temp += (vmax - vmin) / vsegments;
+                // X and Z coordinates are on a circle around the origin
+                var x = this.posFunc(umin_temp, vmin_temp)[0];
+                var y = this.posFunc(umin_temp, vmin_temp)[1];
+                var z = this.posFunc(umin_temp, vmin_temp)[2];
+                coords.push(x,y,z);
+
+            };
+
+
+        };
+
+        this.coordsBuffer = new vbo.Attribute(gl, { "numComponents": 3,
+                                                    "dataType": gl.FLOAT,
+                                                    "data": coords 
+                                                  } );
+
+       
     };  
 
     // draw method: activate buffers and issue WebGL draw() method
     ParametricSurface.prototype.draw = function(gl,program) {
-    
-        window.console.log("ParametricSurface.draw() not implemented yet.")
 
+        // bind the attribute buffers
+        program.use();
+        this.coordsBuffer.bind(gl, program, "vertexPosition");
+ 
+        // draw the vertices as points
+        gl.drawArrays(gl.POINTS, 0, this.coordsBuffer.numVertices()); 
+       
+        
     };
         
     // this module only returns the Band constructor function    
